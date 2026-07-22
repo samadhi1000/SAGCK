a112e05504ab3787ccf3f697196de1412075c2b5
samadhi1000 <samadhi1000@gmail.com>
2026-07-23 00:05:04 +0530

    fix: update hero-4 image file extension from .png to .jpg

--- src/components/HeroBackground.tsx
+++ src/components/HeroBackground.tsx
@@ -7,7 +7,7 @@ const HERO_IMAGES = [
   "/images/hero/hero-1.jpg",
   "/images/hero/hero-2.jpg",
   "/images/hero/hero-3.jpg",
-  "/images/hero/hero-4.png",
+  "/images/hero/hero-4.jpg",
   "/images/hero/hero-5.jpg",
 ];
 
